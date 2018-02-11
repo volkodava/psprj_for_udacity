9dec731bb73f8d12f0353848bf25b2da2f0971fb
Anatolii Volkodav <volkodav@ngti.nl>
2018-02-11 20:06:42 +0100

    init

---
+++ polyspace/Module_1/BF_Result/.settings/launchingCommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/Applications/MATLAB_R2017b.app/polyspace/bin/polyspace-bug-finder-nodesktop" -lang CPP -options-file "/Users/volkodav/self_driving_car_proj/psprj_for_udacity/polyspace/Module_1/BF_Result/.settings/options_command.txt" -results-dir "/Users/volkodav/self_driving_car_proj/psprj_for_udacity/polyspace/Module_1/BF_Result" $*
